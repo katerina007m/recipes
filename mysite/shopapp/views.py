0ea1a02b5dc504e9556cb7f209698fd10f9c8df8
katerina007m <katerina007m@mail.ru>
2025-02-13 16:32:17 +0300

    work with db

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from timeit import default_timer
 from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.models import Group
 
 
 def shop_index(request: HttpRequest):
@@ -15,3 +16,9 @@ def shop_index(request: HttpRequest):
         "products": products,
     }
     return render(request, 'shopapp/shop-index.html', context=context)
+
+def groups_list(request: HttpRequest):
+    context = {
+        "groups": Group.objects.prefetch_related('permissions').all(),
+    }
+    return  render(request, 'shopapp/shop-index.html', context=context)
