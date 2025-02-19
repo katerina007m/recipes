e0671a78a75b72c78a70e218fdb67a4e1473afcf
katerina007m <katerina007m@mail.ru>
2025-02-19 16:37:38 +0300

    work with requestdataapp

--- mysite/requestdataapp/apps.py
+++ mysite/requestdataapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RequestdatappConfig(AppConfig):
+class RequestdataappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'requestdatapp'
+    name = 'requestdataapp'
---
+++ mysite/requestdataapp/views.py
@@ -0,0 +1,17 @@
+from django.db.models.expressions import result
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def process_get_view(request: HttpRequest) -> HttpResponse:
+    a = request.GET.get("a", "")
+    b = request.GET.get("b", "")
+    result = a + b
+    context = {
+        "a": a,
+        "b": b,
+        "result": result,
+    }
+    return render(request, "requestdataapp/request-query-params.html", context=context)
