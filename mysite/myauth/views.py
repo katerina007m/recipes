4fb25f874b202dddcc224c706ea8b9c895649512
katerina007m <katerina007m@mail.ru>
2025-03-04 15:05:19 +0300

    work myauth

---
+++ mysite/myauth/views.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.views import LogoutView
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render,redirect
+from django.urls import reverse, reverse_lazy
+from django.views import View
+from django.contrib.auth import authenticate, login, logout
+
+
+def login_view(request: HttpRequest) -> HttpResponse:
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect('/admin/')
+        return render(request, 'myauth/login.html')
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('/admin/')
+    return render(request, 'myauth/login.html', {'error': 'Invalid username or password'})
+
+def logout_view(request: HttpRequest) -> HttpResponse:
+    logout(request)
+    return redirect(reverse('myauth:login'))
+
+class MyLogoutView(LogoutView):
+    next_page = reverse_lazy('myauth:login')
+
+def set_cookie_view(request: HttpRequest) -> HttpResponse:
+    response = HttpResponse("Cookies set")
+    response.set_cookie("fizz", "buzz", max_age=3600)
+    return response
+
+def get_cookie_view(request: HttpRequest) -> HttpResponse:
+    value = request.COOKIES.get("fizz", "default value")
+    return HttpResponse(f"Cookies value: {value!r}")
+
+def set_session_view(request: HttpRequest) -> HttpResponse:
+    request.session["foobar"]="spameggs"
+    return HttpResponse("Session set")
+
+
+def get_session_view(request: HttpRequest) -> HttpResponse:
+    value = request.session.get("foobar", "default")  # сохраняем значение в переменной value
+    return HttpResponse(f"Session value: {value!r}")  # используем value в ответе
+
