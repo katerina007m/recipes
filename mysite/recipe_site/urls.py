0129e3dbaf4c17fd5a6a888b534c0126fb2b9fcc
katerina007m <katerina007m@mail.ru>
2025-03-27 08:40:26 +0300

    Application for receipts added

---
+++ mysite/recipe_site/urls.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+from myauth.views import MyLoginView, RegisterView, logout_view
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("recipe/<int:pk>/", views.recipe_detail, name="recipe_detail"),
+    path("recipe/add/", views.recipe_add, name="recipe_add"),
+    path("login/", MyLoginView.as_view(redirect_authenticated_user=True), name="login"),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("logout/", logout_view, name="logout"),
+    path("recipe/delete/<int:pk>/", views.recipe_delete, name="recipe_delete"),
+    path("recipe/edit/<int:pk>/", views.recipe_edit, name="recipe_edit"),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
