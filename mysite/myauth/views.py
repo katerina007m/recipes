a84d3173005cc7beb6a40285e54d3c82cb468709
katerina007m <katerina007m@mail.ru>
2025-03-28 18:03:58 +0300

    fix of form and reqs

--- mysite/myauth/views.py
+++ mysite/myauth/views.py
@@ -1,12 +1,11 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, CreateView
 
-from .models import Profile
+from .models import Profile, CustomUserCreationForm
 
 
 class AboutMeView(TemplateView):
@@ -14,23 +13,25 @@ class AboutMeView(TemplateView):
 
 
 class RegisterView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     template_name = "myauth/register.html"
     success_url = reverse_lazy("myauth:about-me")
 
     def form_valid(self, form):
         response = super().form_valid(form)
-        Profile.objects.create(user=self.object)
+        Profile.objects.create(
+            user=self.object,
+            bio=form.cleaned_data.get("bio", ""),
+        )
         username = form.cleaned_data.get("username")
         password = form.cleaned_data.get("password1")
-
         user = authenticate(
             self.request,
             username=username,
             password=password,
         )
-        login(request=self.request, user=user)
-
+        if user:
+            login(request=self.request, user=user)
         return response
 
 
